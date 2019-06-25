f27c54784617c1e12bf08f3ad558200ec6318b83
johnmtay <tay18008@byui.edu>
2019-06-25 13:36:38 -0600

    fixed contacts pipe

--- cms/src/app/contacts/contacts-filter.pipe.spec.ts
+++ cms/src/app/contacts/contacts-filter.pipe.spec.ts
@@ -1,8 +1,25 @@
-import { ContactsFilterPipe } from './contacts-filter.pipe';
+import { Pipe, PipeTransform } from '@angular/core';
+import { Contact } from './contact.model';
 
-describe('ContactsFilterPipe', () => {
-  it('create an instance', () => {
-    const pipe = new ContactsFilterPipe();
-    expect(pipe).toBeTruthy();
-  });
-});
+@Pipe({
+  name: 'contactsFilter'
+})
+export class ContactsFilterPipe implements PipeTransform {
+
+  
+  transform(contacts: Contact[], term: string): any {
+    let filteredArray: Contact[] = [];
+
+    if (term && term.length > 0) {
+      filteredArray = contacts.filter(
+        (contact: Contact) => contact.name.toLowerCase().includes(term.toLowerCase())
+      );
+    }
+
+    if (filteredArray.length < 1) {
+      return contacts;
+    }
+
+    return filteredArray;
+  }
+}
